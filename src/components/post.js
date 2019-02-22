5b9b5181fe6c90233010dd2bd3512b0006ffaca9
denebtech <techdeneb@gmail.com>
2019-02-22 13:49:03 -0500

    modified post components to render a result post and setup render links

--- src/components/post.js
+++ src/components/post.js
@@ -9,17 +9,48 @@ class Post extends Component {
         return topics;
     }
 
+    renderLinks() {
+        let links = this.props.post_links.map((post_link, index) => {
+            return (
+                <div className="post-link" key={index}>
+                    <div className="post-link__box"></div>
+                    <div className="post-link__link">
+                        <a href={post_link.link_url}>Useful Link #{index + 1}</a>
+                    </div>
+                </div>
+            )
+        })
+        return links;
+    }
+
     render() {
-        return (
-            <li className="recent-post">
-                <div className="recent-post__title">
-                    {this.props.title}
-                </div>
-                <div className="recent-post__topics">
-                    {this.renderTopics()}
-                </div>
-            </li>
-        )
+        if(this.props.type == 'recent') {
+            return (
+                <li className="recent-post">
+                    <div className="recent-post__title">
+                        {this.props.title}
+                    </div>
+                    <div className="recent-post__topics">
+                        {this.renderTopics()}
+                    </div>
+                </li>
+            )
+        } else if(this.props.type == 'result') {
+            return (
+                <li className="result-post">
+                    <div className="result-post__topics">
+                        {this.renderTopics()}
+                    </div>
+                    <div className="result-post__title">
+                        {this.props.title}
+                    </div>
+                    <div className="result-post__links">
+                        {this.renderLinks()}
+                    </div>
+                </li>
+            )
+        }
+
     }
 }
 
--- src/components/resultsPosts.js
+++ src/components/resultsPosts.js
@@ -7,7 +7,8 @@ class ResultsPosts extends Component {
 
     renderPosts() {
         const posts = this.props.posts.map((post, index) => {
-            return <Post key={index} {...post}/>
+            return <Post type="result" key={index} {...post}/>
+
         })
         return posts;
     }
