49ad43e2764728d002ca18bde1bc6564a81e27e2
denebtech <techdeneb@gmail.com>
2019-02-21 20:25:30 -0500

    setup results component

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -11,15 +11,15 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './style/main.scss';
 
 import Home from './components/home'
-
+import Results from './components//results';
 
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
         <Switch>
-          <Route path='/' component={Home}/>
-          <Route path='/results' component={Home}/>
+          <Route path='/' exact component={Home}/>
+          <Route path='/results' component={Results}/>
         </Switch>
       </BrowserRouter>
     </Provider>
---
+++ src/components/results.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Logo from './logo';
+
+class Results extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Results</h1>
+                <Logo/>
+            </div>
+        )
+    }
+}
+
+export default Results;
